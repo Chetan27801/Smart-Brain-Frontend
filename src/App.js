1ab9cf6ac9315b8ea6b16643cd833a6fe02a2dbc
Chetan <c27801j@gmail.com>
2023-10-08 17:16:53 +0530

    clarify api on frontend

--- src/App.js
+++ src/App.js
@@ -9,6 +9,42 @@ import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
 import "./App.css";
 
+const returnClarifaiRequestOptions = (imageUrl) => {
+	const PAT = "15646a891cd34cdba5e25f5b9b6f3863";
+	const USER_ID = "oq5j7ugcxd3r";
+	const APP_ID = "test";
+	// const MODEL_ID = "face-detection";
+	// const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+	const IMAGE_URL = imageUrl;
+
+	const raw = JSON.stringify({
+		user_app_id: {
+			user_id: USER_ID,
+			app_id: APP_ID,
+		},
+		inputs: [
+			{
+				data: {
+					image: {
+						url: IMAGE_URL,
+					},
+				},
+			},
+		],
+	});
+
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			Authorization: "Key " + PAT,
+		},
+		body: raw,
+	};
+
+	return requestOptions;
+};
+
 const initialState = {
 	input: "",
 	imageUrl: "",
@@ -75,13 +111,10 @@ class App extends Component {
 	onButtonSubmit = (event) => {
 		this.setState({ imageUrl: this.state.input });
 
-		fetch("http://localhost:3000/imageurl", {
-			method: "post",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				input: this.state.input,
-			}),
-		})
+		fetch(
+			"https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+			returnClarifaiRequestOptions(this.state.input)
+		)
 			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
@@ -113,8 +146,8 @@ class App extends Component {
 				<div className="flex justify-between">
 					<Logo />
 					<Navigation
-					isSignedIn={isSignedIn}
-					onRouteChange={this.onRouteChange}
+						isSignedIn={isSignedIn}
+						onRouteChange={this.onRouteChange}
 					/>
 				</div>
 				{route === "home" ? (
